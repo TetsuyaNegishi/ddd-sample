f757ee1d72d1d1b091d804868300700a6add99d2
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2021-07-11 15:30:17 +0900

    chore: expressの環境整備

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import express from "express";
 import { hello } from "./hello";
 
-console.log(hello("World"));
+const app: express.Express = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router: express.Router = express.Router();
+router.get("/api/test", (req: express.Request, res: express.Response) => {
+  res.send(hello("world"));
+});
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
